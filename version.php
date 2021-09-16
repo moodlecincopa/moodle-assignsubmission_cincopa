4b8843e6c97ef6ddf7b5f044c213ff26ca5430f1
Davit Harut <davit@cincopa.com>
2021-09-16 18:00:30 +0400

    Updated plugin version to 1.0.3

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018041700;		// The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021091600;		// The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;		// Requires this Moodle version.
 $plugin->component = 'assignsubmission_cincopa';	// Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.2';
+$plugin->release   = 'v1.0.3';
